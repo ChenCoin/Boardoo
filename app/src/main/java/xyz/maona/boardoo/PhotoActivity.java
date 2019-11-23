130971f14bf0282feba16df8d7ac9926b3729dc3
ChenCoin <chencoin@qq.com>
2019-11-23 22:50:17 +0800

    change dependence and add menu

--- app/src/main/java/xyz/maona/boardoo/PhotoActivity.java
+++ app/src/main/java/xyz/maona/boardoo/PhotoActivity.java
@@ -1,19 +1,25 @@
 package xyz.maona.boardoo;
 
+import android.animation.ObjectAnimator;
 import android.app.Activity;
 import android.content.ClipData;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.TextView;
-import com.bm.library.PhotoView;
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import android.widget.Toast;
+
+import com.github.chrisbanes.photoview.PhotoView;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -22,52 +28,95 @@ public class PhotoActivity extends Activity {
 
     private String FILE_NAME = "cache.png";
     private PhotoView photoView;
-    private TextView tip;
+    private View index;
+    View background;
+    private View menu;
+    private View menuBox;
+    private View loading;
+    private boolean menuShowing = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-            WindowManager.LayoutParams lp = getWindow().getAttributes();
-            lp.layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
-            getWindow().setAttributes(lp);
-        }
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        config();
         setContentView(R.layout.activity_photo);
         photoView = findViewById(R.id.photoView);
-        tip = findViewById(R.id.tip);
-        photoView.enable();
+        index = findViewById(R.id.index);
+        background = findViewById(R.id.background);
+        menu = findViewById(R.id.menu);
+        menuBox = findViewById(R.id.menu_box);
+        loading = findViewById(R.id.loading);
 
-        View menuContainer = findViewById(R.id.menu);
-        findViewById(R.id.cancel).setOnClickListener(view -> {
-            menuContainer.setVisibility(View.GONE);
+        findViewById(R.id.cancel).setOnClickListener(view -> hideMenu());
+        findViewById(R.id.delete).setOnClickListener(view -> {
+            File file = new File(getCacheDir(), FILE_NAME);
+            if (!file.exists() || !file.delete()) {
+                Toast.makeText(this, getResources().getString(R.string.error),
+                        Toast.LENGTH_SHORT).show();
+            }
+            hideMenu();
+            visibility(index);
         });
-//        photoView.setOnLongClickListener(view -> {
-//            menuContainer.setVisibility(View.VISIBLE);
-//            return true;
-//        });
+        photoView.setOnLongClickListener(this::showMenu);
+    }
+
+    private void config() {
+        View decorView = getWindow().getDecorView();
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+        } else if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
+            decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
+                    View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        } else {
+            decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
+                    View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR |
+                    View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR);
+        }
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        tip.setText(R.string.loading);
+        visibility(loading);
         updatePhoto(this::loadLocalPhoto);
     }
 
+    @Override
+    public void onBackPressed() {
+        if (menuShowing) hideMenu();
+        else super.onBackPressed();
+    }
+
+    private void visibility(View show) {
+        photoView.setVisibility(View.INVISIBLE);
+        index.setVisibility(View.INVISIBLE);
+        menu.setVisibility(View.INVISIBLE);
+        loading.setVisibility(View.INVISIBLE);
+        show.setVisibility(View.VISIBLE);
+    }
+
     private void loadPhoto(File file) {
-        photoView.setVisibility(View.VISIBLE);
-        tip.setVisibility(View.INVISIBLE);
-        Glide.with(this).load(file).skipMemoryCache(true)
-                .diskCacheStrategy(DiskCacheStrategy.NONE).into(photoView);
+        try {
+            Bitmap bitmap = BitmapFactory.decodeStream(new FileInputStream(file));
+            photoView.setImageBitmap(bitmap);
+            visibility(photoView);
+        } catch (FileNotFoundException ignored) {
+            visibility(index);
+            Toast.makeText(this, getResources().getString(R.string.error),
+                    Toast.LENGTH_SHORT).show();
+        }
     }
 
     private void loadLocalPhoto() {
         File file = new File(getCacheDir(), FILE_NAME);
         if (!file.exists()) {
-            tip.setText(R.string.tip);
-            photoView.setVisibility(View.INVISIBLE);
-            tip.setVisibility(View.VISIBLE);
+            visibility(index);
         } else loadPhoto(file);
     }
 
@@ -111,4 +160,28 @@ public class PhotoActivity extends Activity {
         }
         if (goNext) also.run();
     }
+
+    private boolean showMenu(View view) {
+        menuShowing = true;
+        menu.setVisibility(View.VISIBLE);
+        background.setVisibility(View.VISIBLE);
+        ObjectAnimator.ofFloat(background, "alpha", 0, 0.6F)
+                .setDuration(300).start();
+        ObjectAnimator.ofFloat(menuBox, "translationY", menuBox.getHeight(), 0)
+                .setDuration(300).start();
+        return true;
+    }
+
+    private void hideMenu() {
+        menuShowing = false;
+        ObjectAnimator.ofFloat(background, "alpha", 0.6F, 0)
+                .setDuration(300).start();
+        ObjectAnimator.ofFloat(menuBox, "translationY", 0, menuBox.getHeight())
+                .setDuration(300).start();
+        new Handler().postDelayed(() -> {
+            menu.setVisibility(View.INVISIBLE);
+            background.setVisibility(View.VISIBLE);
+        }, 300);
+    }
+
 }
